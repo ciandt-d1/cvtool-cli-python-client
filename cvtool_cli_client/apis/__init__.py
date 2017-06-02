2d38b45e1d04eb4b3fb9d15ea9ff4bcf1abe462f
fabito <fuechi@ciandt.com>
2017-06-01 23:37:26 -0300

    initial version

---
+++ cvtool_cli_client/apis/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .auth_api import AuthApi
+from .default_api import DefaultApi
+from .image_api import ImageApi
+from .job_api import JobApi
+from .project_api import ProjectApi
+from .tenant_api import TenantApi
